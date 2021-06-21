dafcc1d239d1188847e12f4927d6585ed5801cae
Layer8Err <dwight.brenner@gmail.com>
2021-06-21 19:03:10 +0000

    GitHub action updated PyPi dist

---
+++ docs/README.md
@@ -0,0 +1,6 @@
+# Laserfiche Swagger API
+
+This package is generated using GitHub Actions.
+
+You can use this package for API calls against your live Laserfiche Cloud account. 
+Visit the [developer center](https://developer.laserfiche.com) for more details.
---
+++ laserfiche_api/api/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from laserfiche_api.api.access_tokens_api import AccessTokensApi
+from laserfiche_api.api.attributes_api import AttributesApi
+from laserfiche_api.api.audit_reasons_api import AuditReasonsApi
+from laserfiche_api.api.entries_api import EntriesApi
+from laserfiche_api.api.field_definitions_api import FieldDefinitionsApi
+from laserfiche_api.api.searches_api import SearchesApi
+from laserfiche_api.api.simple_searches_api import SimpleSearchesApi
+from laserfiche_api.api.tag_definitions_api import TagDefinitionsApi
+from laserfiche_api.api.tasks_api import TasksApi
+from laserfiche_api.api.template_definitions_api import TemplateDefinitionsApi
