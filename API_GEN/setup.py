cf14117b6a34ad6f42179cb401b4ab9eddee6560
Layer8Err <dwight.brenner@gmail.com>
2021-10-08 11:21:04 -0400

    Update setup.py
    set version to 1.0.9

--- API_GEN/setup.py
+++ API_GEN/setup.py
@@ -18,7 +18,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 NAME = "laserfiche_api"
-VERSION = "1.0.8"
+VERSION = "1.0.9"
 # To install the library, run the following
 #
 # python setup.py install
